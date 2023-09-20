138813b08d643f4c1c2f471ad15ebc498377e363
szapodea <szapodea@purdue.edu>
2023-09-20 17:01:30 -0400

    labeleling complete

--- NBC.py
+++ NBC.py
@@ -7,6 +7,7 @@ def pre_process(file):
     data = pd.read_csv(file)
     data = replace_quotes(data=data)
     data = to_lowercase(data=data)
+    data.to_csv('./after_lowercase.csv')
     data = numeric_values(data=data)
 
 # function that strips single quotes (') of three dimensions of the data set.
@@ -44,26 +45,61 @@ def to_lowercase(data):
 # function that encodes 4 dimensions into numerical values
 # also outputs the value assigned to certain labels
 def numeric_values(data):
+    RACE = 4 # column of race
+    RACE_O = 5 # column of race_o
+    GENDER = 1 # column of gender
+    FIELD = 9 # column of field
 
-    data = data.sort_values(by=['race', 'age', ], axis=0, ascending=True)
-    print(data['race'])
+    data = data.sort_values(by=['race'])
+    race_dict = {}
+    cnt = 0
 
-    race_dict = {data['race'][0]: 0}
-
-    cnt = 1
-    for i in range(1, len(data['race'])):
-        print(data['race'][i], data['age'][i])
-        if data['race'][i] != data['race'][i-1] and data['race'][i] not in race_dict:
-            race_dict[data['race'][i]] = cnt
+    for row in data.itertuples():
+        if row[RACE] not in race_dict:
+            race_dict[row[RACE]] = cnt
             cnt += 1
 
-    print(race_dict)
+    data = data.replace({'race': race_dict})
 
+    data = data.sort_values(by=['race_o'])
+    raceo_dict = {}
+    cnt = 0
+
+    for row in data.itertuples():
+        if row[RACE_O] not in raceo_dict:
+            raceo_dict[row[RACE_O]] = cnt
+            cnt += 1
+
+    data = data.replace({'race_o': raceo_dict})
+
+    data = data.sort_values(by=['gender'])
+    gender_dict = {}
+    cnt = 0
+
+    for row in data.itertuples():
+        if row[GENDER] not in gender_dict:
+            gender_dict[row[GENDER]] = cnt
+            cnt += 1
+
+    data = data.replace({'gender': gender_dict})
+
+    data = data.sort_values(by=['field'])
+    field_dict = {}
+    cnt = 0
+
+    for row in data.itertuples():
+        if row[FIELD] not in field_dict:
+            field_dict[row[FIELD]] = cnt
+            cnt += 1
+
+    data = data.replace({'field': field_dict})
+
+    print('Value assigned for male in column gender: {0}'.format(gender_dict['male']))
+    print('Value assigned for European/Caucasian-American in column race: {0}.'.format(race_dict['European/Caucasian-American']))
+    print('Value assigned for Latino/Hispanic American in column race_o: {0}.'.format(raceo_dict['Latino/Hispanic American']))
+    print('Value assigned for law in column field: {0}'.format(field_dict['law']))
+    
     return data
-
-
-
-
 
 
 
