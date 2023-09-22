58dc3bcff69dd83006eb563db7b010f09d1cbf30
szapodea <szapodea@purdue.edu>
2023-09-21 21:32:59 -0400

    Scatter plots done

--- Visualization.py
+++ Visualization.py
@@ -34,6 +34,57 @@ def visualize_gender(data):
     plt.show()
 
 
+def scatter_plots(data):
+    cols = ['attractive_partner', 'sincere_partner', 'intelligence_partner',
+            'funny_partner', 'ambition_partner', 'shared_interests_partner']
+
+    distinct_vals = {}
+
+    for col in cols:
+        distinct_vals[col] = data[col].unique()
+    '''
+    print(distinct_vals['attractive_partner'])
+
+    matching = data.loc[data['attractive_partner'] == 0]
+    yes = matching['decision'].value_counts()
+    print(dict(yes))
+    yes = dict(yes)
+    if 1 not in yes.keys():
+        print(0)
+    elif 1 not in yes.keys():
+        print(1)
+    else:
+        print(yes[1]/(yes[0] + yes[1]))
+    '''
+
+    second_date_percent = []
+    for col in cols:
+        temp = {}
+        for val in distinct_vals[col]:
+            matching = data.loc[data[col] == val]
+            cnt = matching['decision'].value_counts()
+            cnt = dict(cnt)
+            if 1 not in cnt.keys():
+                temp[val] = 0
+            elif 0 not in cnt.keys():
+                temp[val] = 1
+            else:
+                temp[val] = cnt[1]/(cnt[0] + cnt[1])
+        second_date_percent.append(temp)
+
+    #print(second_date_percent)
+
+    for col in range(len(cols)):
+        #print(second_date_percent[col].keys())
+        x = second_date_percent[col].keys()
+        y = second_date_percent[col].values()
+        print(x, y)
+        plt.figure(cols[col])
+        plt.scatter(x, y)
+        plt.xlabel('{0} Rating'.format(cols[col]))
+        plt.ylabel('Success Rate')
+
+        plt.show()
 
 
 
@@ -42,4 +93,5 @@ def visualize_gender(data):
 
 if __name__ == '__main__':
     data = pd.read_csv('./dating.csv')
-    visualize_gender(data=data)
+    #visualize_gender(data=data)
+    scatter_plots(data=data)
